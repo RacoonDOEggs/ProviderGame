826d41f1411d60947d64162c65ee1615d8e5c587
Marc-Olivier Beaulieu <beaulieumolivier@gmail.com>
2024-02-06 11:43:35 -0500

    make ui visible when clicked

--- scenes/objects/electrical_box_object.gd
+++ scenes/objects/electrical_box_object.gd
@@ -6,11 +6,13 @@ var mouse_inside:bool = false
 
 func _on_mouse_entered():
 	$WhiteRectangle.visible = true
+	mouse_inside = true
 
 
 func _on_mouse_exited():
 	$WhiteRectangle.visible = false
+	mouse_inside = false
 
 func _process(_delta):
-	if mouse_inside && Input.is_action_just_pressed("clicked"):
+	if Input.is_action_just_pressed("clicked") && mouse_inside:
 		electrical_box_clicked.emit()
--- scenes/ui/electrical_box_ui.gd
+++ scenes/ui/electrical_box_ui.gd
@@ -2,4 +2,4 @@ extends CanvasLayer
 
 
 func _on_electrical_box_object_electrical_box_clicked():
-	print("make ui visible")
+	visible = true
