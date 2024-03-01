82792023e63813a51138977430e26b563439d78f
rimeelb05 <202234795@cegepsherbrooke.qc.ca>
2024-03-01 10:26:13 -0500

    added logic puzzle

---
+++ scenes/objects/closeButton.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#connect("pressed", Callable, "_on_CloseButton_pressed")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_CloseButton_pressed():
+	#var stringNumber = "42"
+	#var integerValue = to_int(stringNumber)
+	get_tree().quit()
---
+++ scenes/ui/puzzle.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_ButtonX_pressed():
+	print ('ButtonX is clicked')
+	#connect doors and strings 
+	
+func _on_ButtonY_pressed():
+	print('ButtonY is clicked')
+	#connect rest
+	
+func _on_ButtonZ_pressed():
+	print('ButtonZ is clicked')
+	#connect rest
+
+#func _on_CloseButton_pressed(event):
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		#get_tree().quit()
+	
