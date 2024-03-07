add7fc5d4f774115b5ee1eac441cea659ea7eb33
Xavier Bilodeau <xavierbilodeau1@gmail.com>
2024-03-07 09:28:02 -0500

    small changes on optical

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,3 +1,15 @@
 extends Node
 
 
+var laser_timer_check: bool = false:
+	get:
+		return laser_timer_check
+	set(value):
+		laser_timer_check = value
+
+
+var angles_value: float:
+	get:
+		return angles_value
+	set(value):
+		angles_value = value
--- scenes/objects/Laser.gd
+++ scenes/objects/Laser.gd
@@ -1,40 +1,71 @@
 #Source pour l'implémentation du laser et des mirroirs : https://www.youtube.com/watch?v=Mgk5eAvzo8k&t=629s
-
 extends StaticBody2D
 
 var max_bounces = 10 
 @onready var ray = $Ray
 @onready var line = $body/Line2D
+var check = false
+var can_win = false
 
 func _process(_delta):
-	
-	# Je supprime tous les points déjà existants dans la scène.
 	line.clear_points()
 	
-	# Si on pèse sur ESPACE ou BOUTON_SOURIS_GAUCHE le rayon s'actionne
+	# Si on pèse sur ESPACE ou F le rayon s'actionne.
 	if Input.is_action_pressed("interact"):
 		
-		#Le premier point est créé sur la ligne.
+		#Le premier point est créé au début de la ligne, donc au début du laser.
 		line.add_point(Vector2.ZERO)
 		
-		# Je défini le début du rayon initial en le plaçant au début de la ligne.
+		# Je défini la position initiale du rayon en le plaçant à la position initiale de la ligne.
 		ray.global_position = line.global_position
 		# La direction du rayon s'oriente en fonction de la position de la souris.
-		ray.target_position = (get_global_mouse_position() - line.global_position).normalized()*10000
-		# Applique les changements
+		ray.target_position = get_local_mouse_position().normalized() * 2000
+		
+		# Met à jour les informations à propos des collisions.
 		ray.force_raycast_update()
 		
-		#L'ancien objet avec qui le rayon a eu une collision.
+		# Vérifie si l'angle du rayon est celui qui permet le rayon de faire une réflexion jusqu'à la zone gagnante(La fibre brisée de droite).
+		# On envoie un signal à la scène du casse-tête pour ensuite partir un compteur.
+		var angles: float = rad_to_deg(ray.target_position.angle())
+
+		# Initialise l'ancien objet avec qui le rayon a eu une collision a nulle.
 		var prev = null
 		# On initialise le nombre de réflexion à 0.
 		var bounces = 0
 		
 		while true:
-			#  Si le rayon n'a pas de collision, on ne veut pas que le laisser disparaîsse, donc on ajoute un point à la ligne pour qu'il apparaîsse.
-			# Puis on quitte la boucle.
+
+
+			# Gagne si le temps est écoulé et que l'angle est bon.
+			if angles <  57 and angles > 54.5 and can_win:
+				print("a")
+				break
+			# Check si l'angle est bon pour partir le temps.
+			else: if angles < 57 and angles > 54.5 :
+				$Timer.start(1)  
+			# Si l'angle n'est pas bon, n'a pas la condition de gagner.
+			else:
+				can_win = false
+
+			#  Si le rayon n'a pas de collision, on ne veut pas le laisser disparaître, donc on ajoute un point à la ligne pour qu'il apparaîsse.
 			if not ray.is_colliding():
-				var pt = ray.global_position + ray.target_position
-				line.add_point(line.to_local(pt))
+				# L'angle de rotation du laser
+				var angle = rad_to_deg(ray.target_position.angle())
+				
+				# Si l'angle se trouve dans la portion acceptée du casse-tête, on le laisse.
+				if angle < 60 and angle > 5:
+					var pt = ray.global_position + ray.target_position
+					line.add_point(line.to_local(pt))
+					
+					# Si l'angle se trouve plus bas que la portion acceptée, on l'arrête.
+				else: if angle > 60:
+					var pt = Vector2(5313.137, 8790.025)
+					line.add_point(line.to_local(pt))
+					
+					# Si l'angle se trouve plus haut que la portion acceptée, on l'arrête.					
+				else: if angle < 5:
+					var pt = Vector2(10199.31, 1063.07)
+					line.add_point(line.to_local(pt))
 				break
 			
 			# On prend en note avec quel objet le rayon a une collision et en quel point.
@@ -81,3 +112,8 @@ func _process(_delta):
 		if prev != null:
 				prev.collision_mask = 3
 				prev.collision_layer = 3
+
+
+func _on_timer_timeout():
+	can_win = true
+	print("timeout")
