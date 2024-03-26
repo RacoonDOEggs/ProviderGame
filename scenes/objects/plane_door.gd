3067dd8a6608799b8563fe41921b71d10e0abd8c
Xavier Bilodeau <xavierbilodeau1@gmail.com>
2024-03-26 18:25:06 -0400

    options added on menu puzzle

--- scenes/objects/plane_door.gd
+++ scenes/objects/plane_door.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 signal player_can_move # Signal indiquant que le joueur peut être en mouvement encore.
 signal plane_door_clicked # Signal indiquant que la porte de l'avion est pesée.
+signal all_puzzle_win2 # Signal indiquant que tous les casse-têtes on été complété.
 
 var mouse_inside:bool = false # Variable indiquant que la souris survole la porte.
 
@@ -18,8 +19,7 @@ func _on_mouse_exited():
 #On émet un signal pour afficher le menu des casse-têtes lorsque la porte est cliquée
 func _input(event):
 	if event.is_action_pressed("clicked") and mouse_inside:
-		plane_door_clicked.emit()
-
+		plane_door_clicked.emit() # On émet un signal vers Menu_puzzle_Ui.
 
 # Message reçu lorsque la porte est cliquée. On affiche le menu des casse-têtes.
 func _on_plane_door_clicked():
@@ -32,13 +32,15 @@ func _on_menu_puzzle_ui_open_electrical_puzzle():
 # Message reçu lorsque le bouton du casse-tête logique est choisi. On affiche le casse-tête logique.
 func _on_menu_puzzle_ui_open_logical_puzzle():
 	$logic_circuit_ui.visible = true
-	
 
 # Message reçu lorsque le bouton du casse-tête optique est choisi. On affiche le casse-tête optique.
 func _on_menu_puzzle_ui_open_optical_puzzle():
 	$Optical_puzzle_ui.visible = true
 
-
 # Signal reçu du menu ,on en renvoie un autre pour avertir le joueur.
 func _on_menu_puzzle_ui_menu_puzzle_exited():
-	player_can_move.emit()
+	player_can_move.emit() # On envoie un autre signal vers test_scene.
+
+# Signal reçcu lorsque tous les casse-têtes on été complété
+func _on_menu_puzzle_ui_all_puzzle_win():
+	all_puzzle_win2.emit() # On envoie un autre signal vers test_scene.
--- scenes/ui/Menu_puzzle_Ui.gd
+++ scenes/ui/Menu_puzzle_Ui.gd
@@ -4,6 +4,8 @@ signal menu_puzzle_exited # Signal émit lorsqu'on ferme le menu des casse-tête
 signal open_electrical_puzzle # Signal émit lorsque le bouton du menu du casse-tête électrique est pesé.
 signal open_logical_puzzle
 signal open_optical_puzzle
+signal all_puzzle_win # Signal indiquant que tous les casse-têtes on été complété.
+
 var can_open_electrical_puzzle = false # Variable indiquant qu'on peut accéder au casse-tête électrique. (Condition: Les résistances(objet dans la carte) ont été rammassé).
 var can_open_logical_puzzle = false # Variable indiquant qu'on peut accéder au casse-tête logique. (Condition: Le casse-tête électrique est complété).
 var can_open_optical_puzzle = false # Variable indiquant qu'on peut accéder au casse-tête optique. (Condition: Les 2 casse-têtes précédents sont complétés).
@@ -11,12 +13,14 @@ var electrical_puzzle_win_check = false # Variable indiquant que le joueur a com
 var logical_puzzle_win_check = false # Variable indiquant que le joueur a complété le casse-tête logique.
 var optical_puzzle_win_check = false # Variable indiquant que le joueur a complété le casse-tête optique.
 
-# Signal reçu lorsque le bouton et émis lorsque le bouton option est pesé.
+func _process(_delta):
+	if electrical_puzzle_win_check and logical_puzzle_win_check and optical_puzzle_win_check:
+		all_puzzle_win.emit() # On émet un signal indiquant que tous les casse-têtes on été complété.
+
+# Signal reçu lorsque le bouton option est pesé.
 func _on_texture_button_pressed():
-	$".".visible = false
-	menu_puzzle_exited.emit()
-
-
+	$Option_panel_for_menu_puzzle.visible = true
+	
 
 # Signal reçu et émis lorsque le bouton du menu du casse-tête électrique est pesé.
 func _on_electrical_but_pressed():
@@ -33,10 +37,13 @@ func _on_optical_but_pressed():
 # Signal reçu lorsqu'on a complété le casse-tête électrique.
 func _on_electrical_box_ui_electrical_win():
 	electrical_puzzle_win_check = true
+	$Logical_button/Logical_but.disabled = false # On active le bouton logique
 
 # Signal reçu lorsqu'on a complété le casse-tête logique.
 func _on_logic_circuit_ui_logical_win():
 	logical_puzzle_win_check = true
+	if !$Optical_button/Optical_but.disabled and !$Logical_button/Logical_but.disabled: # Si le casse-tête électrique et logique ont été complété, on a active le bouton optique..
+		$Optical_button/Optical_but.disabled = false # On active le bouton optique.
 
 # Signal reçu lorsqu'on a complété le casse-tête optique.
 func _on_optical_puzzle_ui_optical_win():
