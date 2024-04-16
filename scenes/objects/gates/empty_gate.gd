06521ad9f64311f12fe18fff3b56ce6fd1d3a8f5
Marc-Olivier Beaulieu <beaulieumolivier@gmail.com>
2024-04-16 10:46:14 -0400

    added light check on gate drop

--- scenes/objects/gates/empty_gate.gd
+++ scenes/objects/gates/empty_gate.gd
@@ -1,8 +1,10 @@
 extends DragAndDrop
 
+signal check_light
 
 func _drop_data(_at_position, data):
 	if data["origin_node"] != self: #Évite un problème où l'objet disparait si il est déposé sur lui même
+		check_light.emit()
 		if data["origin_object_value"] != 0:
 			data["origin_node"].texture = null
 			data["origin_node"].object_id = 0
